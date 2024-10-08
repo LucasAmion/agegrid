0fa8ee8dd18005a0aac17b8bfed6812b4b4bb5af
Lucas Amion <lucas98_amion@hotmail.com>
2024-10-08 00:52:06 -0300

    add find_packages to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='agegrid',
     version = "2024.0.0",
+    packages=find_packages()
     install_requires=[
       "numpy",
       "xarray",
