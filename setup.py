f8ef2dec7722a62fe3acae6b32e282e55a90dadf
Lucas Amion <lucas98_amion@hotmail.com>
2024-10-08 00:53:01 -0300

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='agegrid',
     version = "2024.0.0",
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=[
       "numpy",
       "xarray",
