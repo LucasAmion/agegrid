7369ac6d4d020d3c7033d2e523cfba56bea06bfd
LucasAmion <lamion@dcc.uchile.cl>
2025-05-26 09:33:12 -0400

    remove gprm submodule, add gprm code as a regular folder instead

--- src/agegrid/automatic_age_grid_seeding.py
+++ src/agegrid/automatic_age_grid_seeding.py
@@ -7,9 +7,9 @@ from joblib import Parallel, delayed
 import tempfile
 import pygmt
 
-from ..GPlatesReconstructionModel.gprm.utils import paleogeography as pg
-from ..GPlatesReconstructionModel.gprm.utils.spatial import get_merged_cob_terrane_polygons, get_merged_cob_terrane_raster
-from ..GPlatesReconstructionModel.gprm.utils.fileio import write_xyz_file
+import gprm.utils.paleogeography as pg
+from gprm.utils.spatial import get_merged_cob_terrane_polygons, get_merged_cob_terrane_raster
+from gprm.utils.fileio import write_xyz_file
 
 from . import reconstruct_by_topologies as rbt
 
